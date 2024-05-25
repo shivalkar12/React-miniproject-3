1543dfa494baef5460e86c0da78923b5672d06ab
shivalkar12 <shivalkarkr.in@gmail.com>
2024-05-25 23:23:43 +0530

    update

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -31,6 +31,8 @@ const NumberSelectorContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: end;
+  margin-right: 30px;
+overflow: hidden;
   .flex {
     display: flex;
     gap: 24px;
@@ -45,13 +47,15 @@ const NumberSelectorContainer = styled.div`
   
 `;
 const Box = styled.div`
-  height: 72px;
-  width: 72px;
+  height: 50px;
+  width: 50px;
   border: 1px solid black;
   display: grid;
   place-items: center;
   font-size: 24px;
   font-weight: 700;
+  margin-right: 10px;
+  overflow: hidden;
   background-color: ${(props) => (props.isSelected ? "black" : "white")};
   color: ${(props) => (!props.isSelected ? "black" : "white")};
 `;
